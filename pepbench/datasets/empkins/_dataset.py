005c40d92b87dd12a6bf48818b94d01f817f6b3e
Robert Richer <robert.richer@fau.de>
2024-08-26 11:53:12 +0200

    added reference_pep to datasets

--- pepbench/datasets/empkins/_dataset.py
+++ pepbench/datasets/empkins/_dataset.py
@@ -268,6 +268,27 @@ class EmpkinsDataset(BaseUnifiedPepExtractionDataset):
             return reference_data_dict[phases[0]]
         return pd.concat(reference_data_dict, names=["phase"])
 
+    @property
+    def reference_pep(self) -> pd.DataFrame:
+        reference_icg = self.reference_labels_icg
+        reference_ecg = self.reference_labels_ecg
+
+        # artefact_ids = pd.concat(
+        #     [reference_icg.reindex(["Artefact"], level="label"), reference_ecg.reindex(["Artefact"], level="label")]
+        # )
+        # artefact_ids = artefact_ids.index.get_level_values("heartbeat_id")
+
+        b_points = reference_icg.xs("ICG", level="channel")
+        qwave_onsets = reference_ecg.xs("ECG", level="channel")
+
+        pep_reference = pd.concat([qwave_onsets, b_points]).sort_index()
+        pep_reference = pep_reference["sample_relative"].unstack("label")
+
+        pep_reference.columns = ["q_wave_onset_sample", "b_point_sample"]
+        pep_reference = pep_reference.assign(pep_sample=-1 * pep_reference.diff(axis=1).dropna(axis=1, how="all"))
+        pep_reference = pep_reference.assign(pep_ms=pep_reference["pep_sample"] / self.sampling_rate_ecg * 1000)
+        return pep_reference
+
     # def calculate_pep_manual_labeled(self, ecg_clean, ecg_whole, heartbeats):
     #     # calculate pep out of the manual labels
     #
