2679d11baac58c4d4e17dc4a064400c9757bf3ad
Robert Richer <robert.richer@fau.de>
2024-11-22 14:15:38 +0100

    fixed typos in algorithm names

--- pepbench/algorithms/ecg.py
+++ pepbench/algorithms/ecg.py
@@ -1,15 +1,13 @@
 """Module for ECG event extraction algorithms."""
 
 from biopsykit.signals.ecg.event_extraction import (
-    QPeakExtractionForounzafar2018,
+    QPeakExtractionForouzanfar2018,
     QPeakExtractionMartinez2004Neurokit,
-    QPeakExtractionSciPyFindPeaksNeurokit,
     QPeakExtractionVanLien2013,
 )
 
 __all__ = [
-    "QPeakExtractionSciPyFindPeaksNeurokit",
-    "QPeakExtractionForounzafar2018",
+    "QPeakExtractionForouzanfar2018",
     "QPeakExtractionMartinez2004Neurokit",
     "QPeakExtractionVanLien2013",
 ]
