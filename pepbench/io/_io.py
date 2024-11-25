ef7b1cc51d137242218d1650c880ff32485781a0
SebastianStuehler <sebastian.stuehler@fau.de>
2024-11-25 14:49:35 +0100

    Methods to extract best_estimator from metric_summary and to obtain the features selected by the model

--- pepbench/io/_io.py
+++ pepbench/io/_io.py
@@ -5,8 +5,9 @@ import pandas as pd
 
 from pepbench.evaluation import ChallengeResults
 from pepbench.utils._types import path_t
+from biopsykit.classification.model_selection import SklearnPipelinePermuter
 
-__all__ = ["load_challenge_results_from_folder", "load_best_performing_algos", "convert_hz_to_ms"]
+__all__ = ["load_challenge_results_from_folder", "load_best_performing_algos", "get_best_pipeline_results", "get_best_estimator", "get_pipeline_steps", "convert_hz_to_ms"]
 
 
 def load_challenge_results_from_folder(
@@ -101,18 +102,20 @@ def load_best_performing_algos(
         n_best: Optional[int] = 5,
 ):
     """
-    Load the best performing B-Point Detection algorithms from folder based on the mean absolute error
+    Load the best performing B-Point Detection algorithms from folder based on their mean absolute error
     
     Parameters
     ----------
     folder_path : str or :class:`pathlib.Path`
         The folder path containing the results.
     n_best: int, optional
+        The amount of algorithms that should be returned. Default: ``5``
 
     Returns
     -------
     pd.Dataframe
         The n_best algorihtms with the lowest mean absolute error as a pd.Dataframe
+
     """
     assert folder_path.is_dir(), f"Folder '{folder_path}' does not exist!"
 
@@ -134,6 +137,121 @@ def load_best_performing_algos(
     best_agg_mean_std = agg_mean_std.xs(key="absolute_error_ms", level="metric").nsmallest(n_best, "mean")
 
     return best_agg_mean_std
+
+def get_best_pipeline_results(
+        pipeline_permuter: SklearnPipelinePermuter,
+        metric: Optional[str] = "mean_absolute_error",
+):  
+    """
+    Get the best performing algorithm from the metric summary of the SklearnPipelinePermuter.
+
+    Parameters
+    ----------
+    pipeline_permuter: class biopsykit.classification.model_selection.SklearnPipelinePermuter
+        The pipeline permuter object containing the prediction results and metric performances.
+    metric: str, optional
+        The metric that was used for scoring in the SklearnPipelinePermuter. Default ``"mean_absolute_error"``
+
+    Returns
+    -------
+    pd.DataFrame
+        The prediction and metric performance results of the best estimator.
+
+    """
+    if metric == "mean_absolute_error":
+        sklearn_metric = "mean_test_neg_mean_absolute_error"
+    else:
+        raise KeyError(f"Specified metric is not implemented yet!")
+    
+    assert sklearn_metric in pipeline_permuter.metric_summary().columns, "Specified metric is not used in the pipeline permuter!"
+    return pipeline_permuter.metric_summary().iloc[pipeline_permuter.metric_summary()["mean_test_neg_mean_absolute_error"].argmin()].to_frame().T
+
+def get_best_estimator(
+        pipeline_permuter: SklearnPipelinePermuter,
+        metric: Optional[str] = "mean_absolute_error",
+):
+    """
+    Get the best estimator object and its underlying pipeline combination.
+
+    Parameters
+    ----------
+    pipeline_permuter: class biopsykit.classification.model_selection.SklearnPipelinePermuter
+        The pipeline permuter object containing the prediction results and metric performances.
+    metric: str, optional
+        The metric that was used for scoring in the SklearnPipelinePermuter. Default ``"mean_absolute_error"``
+
+    Returns
+    -------
+    tuple of _PipelineWrapper and tuple
+        The results as a tuple containing of the biopsykit.classification.utils._PipelineWrapper object and a tuple of 
+        strings containing the underlying pipeline combination.
+    """
+
+    if metric == "mean_absolute_error":
+        sklearn_metric = "mean_test_neg_mean_absolute_error"
+    else:
+        raise KeyError(f"Specified metric is not implemented yet!")
+    
+    assert sklearn_metric in pipeline_permuter.metric_summary().columns, "Specified metric is not used in the pipeline permuter!"
+    best_estimator_name = pipeline_permuter.metric_summary().iloc[pipeline_permuter.metric_summary()[sklearn_metric].argmin()].name
+    return pipeline_permuter.best_estimator_summary().loc[best_estimator_name].values[0], best_estimator_name
+
+def get_pipeline_steps(
+        pipeline_permuter: SklearnPipelinePermuter,
+        input_data: pd.DataFrame,
+        metric: Optional[str] = "mean_absolute_error",
+        step: Optional[str] = "reduce_dim",
+        scaler: Optional[bool] = True,
+):
+    """
+    Gain further insights in the components of your model. E.g. recieve the features selected by the model.
+
+    Parameters
+    ----------
+    pipeline_permuter: class biopsykit.classification.model_selection.SklearnPipelinePermuter
+        The pipeline permuter object containing the prediction results and metric performances.
+    input_data: pd.DataFrame
+        The data that was used to train the models.
+    metric: str, optional
+        The metric that was used for scoring in the SklearnPipelinePermuter. Default ``"mean_absolute_error"``
+    step: str, optional
+        The step of interest. Default ``"reduce_dim"``
+    scaler: str, optional
+        Specifies whether scaling was considered in the model. Default ``True``
+
+    Results
+    -------
+    list
+        List of selected features for each pipeline in the best_estimator object.
+    
+    """
+    best_estimator, best_estimator_name = get_best_estimator(pipeline_permuter=pipeline_permuter, metric=metric)
+    reduce_dim_model = ""
+    selected_features = []
+    selected_feature_mask = None
+
+    if scaler:
+        if step == "reduce_dim":
+            reduce_dim_model = best_estimator_name[1]
+        else:
+            raise KeyError("Specified step is not supported yet!")
+    else:
+        if step == "reduce_dim":
+            reduce_dim_model = best_estimator_name[0]
+        else:
+            raise KeyError("Specified step is not supported yet!")
+    
+    for pipeline in best_estimator.pipeline:
+        if reduce_dim_model == "RFE":
+            selected_feature_mask = pipeline.named_steps["reduce_dim"].support_
+        elif reduce_dim_model == "SelectKBest":
+            selected_feature_mask = pipeline.named_steps["reduce_dim"].get_support()
+        else:
+            raise KeyError("The model used in your pipeline is not supported yet!")
+        selected_features.append(input_data.columns[selected_feature_mask].to_list())
+    return selected_features
+    
+    
 
 def convert_hz_to_ms(sampling_frequency):
     """
