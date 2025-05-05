e2f91ad9052ee6f7c7765623fd481615b9d1abff
SebastianStuehler <sebastian.stuehler@fau.de>
2025-05-05 09:22:36 +0200

    Added hpc scripts

---
+++ experiments/pep_algorithm_benchmarking/notebooks/hpc_scripts/jobscript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+# first non-empty non-comment line ends PBS options
+
+# load bash config
+source etc/.bashrc
+
+module load python/3.9-anaconda
+
+# jobs always start in submit directory - change to work directory
+cd "$WORK"/pepbench || exit
+# activate the venv environment stored in the ".venv" directory
+source .venv/bin/activate
+# change into script directory
+cd experiments/pep_algorithm_benchmarking/notebooks/hpc_scripts || exit
+# set environment variable to disable outdated package
+export OUTDATED_IGNORE=1
+
+# run (all parameters are passed via environment variables)
+python "$FILE_NAME.py"
---
+++ experiments/pep_algorithm_benchmarking/notebooks/hpc_scripts/submit_job.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import subprocess
+import sys
+from pathlib import Path
+
+from hpc_helper import build_job_submit_slurm, check_interpreter
+
+# print path of python interpreter
+print(sys.executable)
+
+#deploy_type = sys.argv[1]
+
+#check_interpreter(deploy_type)
+
+job_list = [
+    {
+         "name": "regression",
+         "type": "general",
+         "walltime": "24:00:00",
+         "random_state": 0,
+    }
+]
+
+print("CURRENT DIRECTORY")
+print(Path().absolute())
+print(f"Length of job_list: {len(job_list)}")
+
+for job_dict in job_list:
+    walltime = job_dict.pop("walltime")
+    file_name = job_dict.pop("name")
+    kwargs = {f"PARAM__{k.upper()}": v for k, v in job_dict.items()}
+
+    job_name = file_name
+    for key, value in job_dict.items():
+        if key in ("previous_job_id", "feature_extraction_job_id"):
+            continue
+        job_name += f"_{key}_{value}"
+
+    qsub_command = build_job_submit_slurm(
+        job_name=job_name,
+        target_system="woody",
+        script_name="jobscript.sh",
+        nodes=1,
+        tasks_per_node=32,
+        walltime=walltime,
+        FILE_NAME=file_name,
+        **kwargs,
+    )
+
+    print(qsub_command)  # Uncomment this line when testing to view the qsub command
+    # Comment the following 3 lines when testing to prevent jobs from being submitted
+    exit_status = subprocess.call(qsub_command, shell=True)
+    if exit_status == 1:  # Check to make sure the job submitted
+        print(f"Job '{qsub_command}' failed to submit")
+
+print("Done submitting all jobs!")
