40f2bfee1cb83edd66b27ea6eba7dd17aedbc6d5
SebastianStuehler <sebastian.stuehler@fau.de>
2025-08-14 18:13:25 +0200

    Working hyperparameter grid for data with nan values

--- experiments/pep_algorithm_benchmarking/notebooks/ML_analysis/regression_scripts/b_point/b_point_regression_with_nan.py
+++ experiments/pep_algorithm_benchmarking/notebooks/ML_analysis/regression_scripts/b_point/b_point_regression_with_nan.py
@@ -16,6 +16,7 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import GroupKFold
 
 from biopsykit.classification.model_selection import SklearnPipelinePermuter
+import sklearn
 
 
 # Set the working directory to the script's directory to ensure expected behavior of the relative paths
@@ -24,15 +25,19 @@ file_name = Path(__file__).with_suffix("").name
 
 data_path = Path("../../results/data")
 models_path = Path("../../results/models")
+rater = "rater_01"
 
+print(sklearn.__version__)
 # Train Regression model for B-Point detection
 
-input_data_b_point = pd.read_csv(data_path.joinpath("b-point/without-rr-interval/train_data_b_point_include_nan.csv"), index_col=[0,1,2,3,4,5])
+input_data_b_point = pd.read_csv(data_path.joinpath(f"b-point/rr-interval/{rater}/train_data_b_point_rr_interval_include_nan.csv"), index_col=[0,1,2,3,4,5]).astype(float)
 
 X_b_point, y_b_point, groups_b_point, group_keys_b_point = bp.classification.utils.prepare_df_sklearn(data=input_data_b_point, label_col="b_point_sample_reference", subject_col="participant", print_summary=False)
+print(f"Input data dtype: {X_b_point.dtype}")
+print(f"y_b_point isnan: {np.isnan(y_b_point).any()}")
 
 model_dict_b_point = {
-    "scaler": {"StandardScaler": StandardScaler(), "MinMaxScaler": MinMaxScaler()},
+    #"scaler": {"StandardScaler": StandardScaler(), "MinMaxScaler": MinMaxScaler()},
     "clf": {
         "DecisionTreeRegressor": DecisionTreeRegressor(),
         "RandomForestRegressor": RandomForestRegressor(),
@@ -40,21 +45,21 @@ model_dict_b_point = {
 }
 
 params_dict_b_point = {
-    "StandardScaler": None,
-    "MinMaxScaler": None,
+    #"StandardScaler": None,
+    #"MinMaxScaler": None,
     "DecisionTreeRegressor": {
-        "criterion": ["squared_error", "friedman_mse", "absolute_error"],
+        "criterion": ["squared_error", "friedman_mse"],
         "splitter": ["best"],
         "max_depth": [4, 8, 16, 32, None],
         "min_samples_leaf": [2, *list(np.arange(10, 100, 10))],
         "min_samples_split": [2, *list(np.arange(10, 100, 10))],
-        "max_features": [*list(np.arange(0.1, 1.0, 0.2)), "log2", "sqrt", None],
+        #"max_features": [*list(np.arange(0.1, 1.0, 0.2)), "log2", "sqrt", None],
     },
     "RandomForestRegressor": {
         "bootstrap": [True],  # Disabling bootstrap often doesn't help much in regression
         "criterion": ["squared_error", "friedman_mse"],
         "max_depth": [*list(np.arange(10, 100, 10)), None],  # Reducing unnecessary fine granularity
-        "max_features": ["sqrt", "log2", 0.2, 0.5, 0.8],  # Balanced choices
+        #"max_features": ["sqrt", "log2", 0.2, 0.5, 0.8],  # Balanced choices
         "min_samples_leaf": [1, 5, 10, 20, 50],  # More practical ranges
         "min_samples_split": [2, 5, 10, 20, 50],  # Logarithmic scaling
         "min_weight_fraction_leaf": [0.0, 0.01, 0.05],  # Only necessary values
@@ -70,7 +75,7 @@ hyper_search_dict = {
     "RandomForestRegressor": {"search_method": "random", "n_iter": 4000},
 }
 
-input_file_path_b_point = models_path.joinpath(f"b-point/without-rr-interval/b_point_{file_name}_hpc_{job_id}.pkl")
+input_file_path_b_point = models_path.joinpath(f"b-point/rr-interval/{rater}/b_point_{file_name}_hpc_{job_id}.pkl")
 if input_file_path_b_point.exists():
     print(f"Loading pre-fitted pipeline permuter from {input_file_path_b_point}")
     pipeline_permuter_b_point = SklearnPipelinePermuter.from_pickle(input_file_path_b_point)
@@ -81,8 +86,8 @@ outer_cv = GroupKFold(n_splits=5)
 inner_cv = GroupKFold(n_splits=5)
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=ConvergenceWarning)
-    pipeline_permuter_b_point.fit_and_save_intermediate(X=X_b_point, y=y_b_point, file_path=models_path.joinpath(f"b-point/without-rr-interval/b_point_{file_name}_hpc_{job_id}_baseline_result_include_nan.pkl"), outer_cv=outer_cv, inner_cv=inner_cv, scoring="neg_mean_absolute_error", groups=groups_b_point)
+    pipeline_permuter_b_point.fit_and_save_intermediate(X=X_b_point, y=y_b_point, file_path=models_path.joinpath(f"b-point/rr-interval/{rater}/b_point_{file_name}_hpc_{job_id}_baseline_result_rr_include_nan_{rater}.pkl"), outer_cv=outer_cv, inner_cv=inner_cv, scoring="neg_mean_absolute_error", groups=groups_b_point)
 
 
-pipeline_permuter_b_point.to_pickle(models_path.joinpath(f"b-point/without-rr-interval/b_point_{file_name}_hpc_{job_id}_baseline_result_include_nan.pkl"))
+pipeline_permuter_b_point.to_pickle(models_path.joinpath(f"b-point/rr-interval/{rater}/b_point_{file_name}_hpc_{job_id}_baseline_result_rr_include_nan_{rater}.pkl"))
 print("Generated pickle file!")
--- experiments/pep_algorithm_benchmarking/notebooks/ML_analysis/regression_scripts/b_point/b_point_regression_without_nan.py
+++ experiments/pep_algorithm_benchmarking/notebooks/ML_analysis/regression_scripts/b_point/b_point_regression_without_nan.py
@@ -33,10 +33,11 @@ file_name = Path(__file__).with_suffix("").name
 
 data_path = Path("../../results/data")
 models_path = Path("../../results/models")
+rater = "rater_01"
 
 # Train Regression model for B-Point detection
 
-input_data_b_point = pd.read_csv(data_path.joinpath("b-point/rr-interval/train_data_b_point_rr_interval.csv"), index_col=[0,1,2,3,4,5])
+input_data_b_point = pd.read_csv(data_path.joinpath(f"b-point/rr-interval/{rater}/train_data_b_point_rr_interval.csv"), index_col=[0,1,2,3,4,5])
 
 X_b_point, y_b_point, groups_b_point, group_keys_b_point = bp.classification.utils.prepare_df_sklearn(data=input_data_b_point, label_col="b_point_sample_reference", subject_col="participant", print_summary=False)
 
@@ -113,7 +114,7 @@ hyper_search_dict = {
     "RandomForestRegressor": {"search_method": "random", "n_iter": 4000},
 }
 
-input_file_path_b_point = models_path.joinpath(f"b-point/rr-interval/b_point_{file_name}_hpc_{job_id}.pkl")
+input_file_path_b_point = models_path.joinpath(f"b-point/rr-interval/{rater}/b_point_{file_name}_hpc_{job_id}.pkl")
 if input_file_path_b_point.exists():
     print(f"Loading pre-fitted pipeline permuter from {input_file_path_b_point}")
     pipeline_permuter_b_point = SklearnPipelinePermuter.from_pickle(input_file_path_b_point)
@@ -124,8 +125,8 @@ outer_cv = GroupKFold(n_splits=5)
 inner_cv = GroupKFold(n_splits=5)
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=ConvergenceWarning)
-    pipeline_permuter_b_point.fit_and_save_intermediate(X=X_b_point, y=y_b_point, file_path=models_path.joinpath(f"b-point/rr-interval/b_point_{file_name}_hpc_{job_id}_baseline_result_rr.pkl"), outer_cv=outer_cv, inner_cv=inner_cv, scoring="neg_mean_absolute_error", groups=groups_b_point)
+    pipeline_permuter_b_point.fit_and_save_intermediate(X=X_b_point, y=y_b_point, file_path=models_path.joinpath(f"b-point/rr-interval/{rater}/b_point_{file_name}_hpc_{job_id}_baseline_result_rr_{rater}.pkl"), outer_cv=outer_cv, inner_cv=inner_cv, scoring="neg_mean_absolute_error", groups=groups_b_point)
 
 
-pipeline_permuter_b_point.to_pickle(models_path.joinpath(f"b-point/rr-interval/b_point_{file_name}_hpc_{job_id}_baseline_result_rr.pkl"))
+pipeline_permuter_b_point.to_pickle(models_path.joinpath(f"b-point/rr-interval/{rater}/b_point_{file_name}_hpc_{job_id}_baseline_result_rr_{rater}.pkl"))
 print("Generated pickle file!")
