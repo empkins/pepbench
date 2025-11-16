b588b79c1243807f7ce90a5fcbd7e6a3094d2333
SebastianStuehler <sebastian.stuehler@fau.de>
2025-11-16 20:53:05 +0100

    Prepared Evaluation notebooks for publication

--- src/pepbench/io/__init__.py
+++ src/pepbench/io/__init__.py
@@ -13,10 +13,9 @@ from pepbench.io._ml_helper import compute_mae_std_from_metric_summary
 from pepbench.io._ml_helper import compute_abs_error
 from pepbench.io._ml_helper import compute_error
 from pepbench.io._ml_helper import impute_missing_values
-from pepbench.io._ml_helper import compute_shap_values
 
 from pepbench.io._io import get_best_estimator
 __all__ = ["load_challenge_results_from_folder", "load_best_performing_algos_b_point", "load_best_performing_algos_q_wave",
            "get_best_pipeline_results", "get_best_estimator", "get_pipeline_steps", "convert_hz_to_ms",
            "load_preprocessed_training_data", "compute_mae_std_from_permuter", "compute_mae_std_from_metric_summary", "compute_abs_error", "compute_error",
-           "impute_missing_values", "compute_shap_values"]
+           "impute_missing_values"]
