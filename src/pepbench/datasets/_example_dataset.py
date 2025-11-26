b5bf3f2769c333ed3ff7206a2157722fd5570d86
Joana <joana.joanidopoulos@web.de>
2025-11-26 12:51:22 +0100

    linking

--- src/pepbench/datasets/_example_dataset.py
+++ src/pepbench/datasets/_example_dataset.py
@@ -17,8 +17,6 @@ from pepbench.datasets import BasePepDatasetWithAnnotations
 from pepbench.datasets._helper import compute_reference_heartbeats
 from pepbench.utils._types import path_t
 
-
-
 HERE = Path(__file__).parent
 EXAMPLE_DATA_PATH = HERE.joinpath("../../../example_data")
 
